fc19af1af036ff919f64bece7a7aec96108b99f3
Katha Patel <42037306+katha-patel@users.noreply.github.com>
2024-07-11 11:35:48 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -12,14 +12,14 @@ resource "aws_s3_bucket_public_access_block" "bucket1" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket = "web-bucket-mathesh"
+  bucket = "web-bucket"
   key    = "index.html"
   source = "index.html"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket = "web-bucket-mathesh"
+  bucket = "web-bucket"
   key    = "error.html"
   source = "error.html"
   content_type = "text/html"
